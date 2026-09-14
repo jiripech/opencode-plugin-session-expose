b3c218e91243224340cf5e307a5c863ff9c8dbad
jiri.pech <git@pech.cz>
2026-09-14 21:18:42 +0200

    Tests for release 1.0.0
    Signed-off-by: jiri.pech <git@pech.cz>

--- tests/session-id.test.ts
+++ tests/session-id.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach } from "vitest"
-import { SessionIdPlugin } from "./opencode/plugins/session-id"
+import { SessionIdPlugin } from "../opencode/plugins/session-id"
 
 describe("SessionIdPlugin", () => {
   it("returns the tool definition", async () => {
